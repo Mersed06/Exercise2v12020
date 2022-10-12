0a299d760e17f7fd7baf90ecf930bb899218d77c
Mersed12 <mersed.keco@stud.fh-campuswien.ac.at>
2022-10-12 21:55:02 +0200

    Excercise 2 done, comments need to be made

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,37 +1,205 @@
 package at.ac.fhcampuswien;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class App {
 
+    Scanner scanner = new Scanner(System.in);
+
+
     //todo Task 1
     public void largestNumber(){
-        // input your solution here
+        System.out.print("Number 1: ");
+        double number = scanner.nextDouble();
+        double largestNumber;
+        largestNumber = number;
+        double secondNumber = number;
+
+        DecimalFormat f1 = new DecimalFormat("0.00");
+
+
+        if (number <= 0) {
+            System.out.println("No number entered.");
+            return;
+        }
+        for (int i = 2; i > 0 && secondNumber > 0; i++) {
+            System.out.print("Number " + i + ": ");
+            secondNumber = scanner.nextDouble();
+            if (secondNumber > number) {
+                largestNumber = secondNumber;
+            }
+        }
+
+        System.out.println("The largest number is " + f1.format(largestNumber));
+
     }
 
     //todo Task 2
     public void stairs(){
-        // input your solution here
-    }
+        System.out.print("n: ");
+        int number = scanner.nextInt();
+        int count = 0;
+        if (number <= 0) {
+            System.out.println("Invalid number!");
+        }
+        for (int i = 1; i <= number; i++) //outer loop
+        {
+            for (int j = 1; j <= i; j++) {
+                count++;
+                System.out.print(count + " ");
+
+            }
+
+            System.out.println();
+        }    }
 
     //todo Task 3
     public void printPyramid(){
-        // input your solution here
-    }
+        int i, j, row = 6;
+        for (i=0; i<row; i++)
+        {
+            for (j=row-i-1; j>0; j--)
+            {
+                System.out.print(" ");
+            }
+            for (j=0; j<i*2+1; j++ )
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }    }
 
     //todo Task 4
-    public void printRhombus(){
-        // input your solution here
+    public void printRhombus() {
+        char a;
+        int height;
+
+        System.out.print("h: ");
+        height = scanner.nextInt();
+        System.out.print("c: ");
+        a = scanner.next().charAt(0);
+        if (height % 2 == 0) {
+            System.out.println("Invalid number!");
+        } else {
+            for (int x = 1; x <= (height / 2) + 1; x++) {
+                for (int y = 1; y <= height - (height / 2) - x; y++) {
+                    System.out.print(" ");
+                }
+
+                for (int z = x; z >= 1; z--) {
+                    System.out.print((char) (a - z + 1));
+                }
+
+                for (int k = 2; k <= x; k++) {
+                    System.out.print((char) (a - k + 1));
+                }
+                System.out.println();
+            }
+
+            for (int x = (height / 2); x >= 1; x--) {
+                for (int y = 1; y <= height - (height / 2) - x; y++) {
+                    System.out.print(" ");
+                }
+
+                for (int z = x; z >= 1; z--) {
+                    System.out.print((char) (a - z + 1));
+                }
+
+                for (int k = 2; k <= x; k++) {
+                    System.out.print((char) (a - k + 1));
+                }
+                System.out.println();
+
+            }
+        }
     }
 
     //todo Task 5
     public void marks(){
-        // input your solution here
+        int count = 0;
+        int add = 0;
+        int negativeMark = 0;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.print("Mark 1: ");
+        int markInput = scanner.nextInt();
+        if (markInput < 0 || markInput > 5) {
+            System.out.println("Invalid mark!");
+            System.out.print("Mark 1: ");
+            markInput = scanner.nextInt();
+
+        } else if (markInput == 0) {
+
+            double average = 0.00;
+            System.out.println("Average: " + df.format(average));
+            System.out.println("Negative marks: " + negativeMark);
+        } else if (markInput == 5) {
+            negativeMark = negativeMark + 1;
+            add = add + markInput;
+            count = count + 1;
+        } else if ((markInput > 0) && (markInput < 6)) {
+            add = add + markInput;
+            count = count + 1;
+        }
+
+        for (int i = 2; markInput != 0; i++) {
+            System.out.print("Mark " + i + ": ");
+            markInput = scanner.nextInt();
+
+            if ((markInput > 0) && (markInput < 6) && (markInput != 5)) {
+                add = add + markInput;
+                count = count + 1;
+
+            } else if (markInput < 0 || markInput > 5) {
+                System.out.println("Invalid mark!");
+                System.out.print("Mark " + i + ": ");
+                markInput = scanner.nextInt();
+                count = count + 1;
+                add = add + markInput;
+
+            } else if (markInput == 0) {
+                double average = (double) add / count;
+                System.out.println("Average: " + df.format(average));
+                System.out.println("Negative marks: " + negativeMark);
+            } else if (markInput == 5) {
+                negativeMark = negativeMark + 1;
+                count = count + 1;
+                add = add + markInput;
+
+            }
+        }
     }
 
     //todo Task 6
     public void happyNumbers(){
-        // input your solution here
+        System.out.print("n: ");
+        int number = scanner.nextInt();
+        int remaining = 0;
+        int sum = 0;
+
+        while(number != 1 || number == 4)
+        {
+            remaining = number % 10;
+            sum = sum + (remaining * remaining);
+            number = number / 10;
+
+            if (number == 0)
+            {
+                number = sum;
+                sum = 0;
+            }
+            else if (sum == 4)
+            {
+                System.out.println("Sad number!");
+                break;
+            }
+        }
+        if (number == 1 )
+        {
+            System.out.println("Happy number!");
+        }
+
     }
 
     public static void main(String[] args){
